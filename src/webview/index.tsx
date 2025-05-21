cb26e593d1c6ff967dd64979c54fe8791c5adbfd
CC11001100 <CC11001100@qq.com>
2025-05-21 22:17:21 +0800

    refactor: 更新webview入口文件，适配新的ConfigPanel组件结构

--- src/webview/index.tsx
+++ src/webview/index.tsx
@@ -1,29 +1,24 @@
+/**
+ * webview/index.tsx
+ * 
+ * WebView入口文件，负责渲染配置面板
+ */
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { ConfigPanel } from './configPanel';
+import { vscode as vscodeApi } from './vscode';
 import './index.css';
-import { ConfigPanel } from './ConfigPanel';
 
-// 使用VSCode的webview通信API
-declare global {
-  interface Window {
-    acquireVsCodeApi: () => {
-      postMessage: (message: any) => void;
-      getState: () => any;
-      setState: (state: any) => void;
-    };
-  }
-}
-
-// 获取VSCode API
-const vscode = window.acquireVsCodeApi();
-
-// 创建根元素并渲染React应用
+// 获取挂载点
 const rootElement = document.getElementById('root');
+
+// 渲染配置面板
 if (rootElement) {
+  // 使用React 18的createRoot API
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <ConfigPanel vscode={vscode} />
+      <ConfigPanel vscode={vscodeApi} />
     </React.StrictMode>
   );
 } 
